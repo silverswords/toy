d28e34d6ce72ea06901f04e60d099051f555b6fb
Feng Yifei <fengyfei@gmail.com>
2020-05-24 22:19:55 +0800

    router

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+  import router from 'page'
+  import Home from './Home.svelte'
+  import About from './About.svelte'
+
+  let page
+
+  router('/', () => page = Home)
+  router('/about', () => page = About)
+
+  router.start()
 </script>
 
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+</nav>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <svelte:component this={ page } />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
