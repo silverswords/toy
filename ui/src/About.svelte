f8e9ba54b9633d030ccab04345962c34996043c7
Feng Yifei <fengyfei@gmail.com>
2020-05-24 23:10:02 +0800

    tailwind samples

--- ui/src/About.svelte
+++ ui/src/About.svelte
@@ -1,3 +1,21 @@
-<div class='w-full bg-gray-200'>
-  <h1>About Page</h1>
+<div class='w-screen bg-gray-200'>
+  <div class='grid grid-cols-2'>
+    <div class='text-center'>
+      <h1>References</h1>
+      <p class='leading-8 text-center'>
+        <a href='https://svelte.dev/'>Svelte</a>
+      </p>
+      <p class='leading-8 text-center'>
+        <a href='https://github.com/visionmedia/page.js'>Page.js</a>
+      </p>
+      <p class='leading-8 text-center'>
+        <a href='https://jackwhiting.co.uk/posts/setting-up-svelte-and-integrating-tailwind-css/'>
+          Installating Svelte & Integrating Tailwind CSS (and PostCSS)
+        </a>
+      </p>
+    </div>
+    <div class='text-center'>
+      <h1>Todos</h1>
+    </div>
+  </div>
 </div>
